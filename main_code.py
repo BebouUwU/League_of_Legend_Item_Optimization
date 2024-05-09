7f2505b26d5f16a149455d1f7fb58776dad1ff33
EstebanDel <estebandelouis2003@gmail.com>
2024-05-09 16:57:02 +0200

    Data modeling and Dataframe creation

--- main_code.py
+++ main_code.py
@@ -20,13 +20,42 @@ data = {"Item_Name" : tableau_de_donnees.keys()}
 
 data = pd.DataFrame(data)
 
+##########################
+### Dataframe creation ###
+##########################
+
 mode = []
+tier = []
+stat = []
+cost = []
 
 for item in tableau_de_donnees:
     colonne = tableau_de_donnees[item]
 
-    mode.append(colonne["modes"])
+    mode.append(colonne.get("modes"))
+    tier.append(colonne.get("tier"))
+    stat.append(colonne.get("stats"))
+    cost.append(colonne.get("buy"))
+
 
 data["mode"] = mode
+data["tier"] = tier
+data["stat"] = stat
+data["cost"] = cost
 
-print(data)
+#################
+# Data modeling #
+#################
+
+#We only keep the items with "Classic 5v5" mode, tier 3
+data.drop(data[data['tier'].isin([1])].index, inplace=True)
+data.drop(data[data['tier'].isin([2])].index, inplace=True)
+data.drop(data[data['tier'].isin([4])].index, inplace=True)
+data.drop(data[data['mode'].isin([{'classic sr 5v5': False, 'aram': True, 'nb': True, 'arena': True},{'classic sr 5v5': False, 'aram': True, 'nb': True, 'arena': False},{'classic sr 5v5': False, 'aram': True, 'nb': False, 'arena': True},{'classic sr 5v5': False, 'aram': True, 'nb': False, 'arena': False},{'classic sr 5v5': False, 'aram': False, 'nb': True, 'arena': True},{'classic sr 5v5': False, 'aram': False, 'nb': True, 'arena': False},{'classic sr 5v5': False, 'aram': False, 'nb': True, 'arena': True},{'classic sr 5v5': False, 'aram': False, 'nb': False, 'arena': True}])].index, inplace=True)
+data = data[pd.to_numeric(data['cost'], errors='coerce').notna()]
+
+
+# Load the datas into an excel file
+data.to_excel("loaded_data.xlsx",index = False)
+
+
